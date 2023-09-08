6afece0e777610d604b1af33823cbe1eed21b6fc
ThaySaad <118492905+ThaySaad@users.noreply.github.com>
2023-09-08 16:18:37 -0300

    Criate Order Dispatched

--- coffee-delivery/src/pages/Checkout/ConfirmedOrder/styles.ts
+++ coffee-delivery/src/pages/Checkout/ConfirmedOrder/styles.ts
@@ -1,21 +1,122 @@
 import styled from "styled-components";
 
- export const MainConfirmedOrder = styled.main``
+export const MainConfirmedOrder = styled.main`
+  padding: 5rem 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: stretch;
+  gap:6.75rem ;
+`;
 
- export const ArticleTitleConfirmedOrder = styled.article``
+export const SectionItemsConfirmedOrder = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: center;
+  gap: 2.5rem;
+`;
 
- export const SectionInformationConfirmationOrder = styled.section``
+export const ArticleTitleConfirmedOrder = styled.article`
+  width: 552px;
 
- export const ArticleDeliveryPlaceConfirmationOrder = styled.article``
+  p:nth-child(1) {
+    color: ${(props) => props.theme["--yellow-dark"]};
+    font-family: "Baloo 2", sans-serif;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: 130%;
+  }
 
- export const FigureLocale = styled.figure``
+  p:nth-child(2) {
+    color: ${(props) => props.theme["--base-subtitle"]};
 
- export const ArticleTimerConfirmationOrder = styled.article``
+    /* Text/Regular L */
+    font-family: Roboto;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%; /* 26px */
+  }
+`;
 
- export const FigureTimer = styled.figure``
+export const SectionInformationConfirmationOrder = styled.section`
+  display: flex;
+  width: 526px;
+  padding: 40px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+  border-radius: 6px 36px;
+  border: 1px solid #dbac2c;
+`;
 
- export const ArticleFormOfPaymentConfirmationOrder = styled.article``
+const BaseItensConfirmationOrder = styled.article`
+  display: flex;
+  align-items: center;
+  gap: 12px;
 
- export const FigureFormOfPayment = styled.figure``
+  p {
+    color: ${(props) => props.theme["--base-text"]};
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+  }
 
-export const SectionIlustration = styled.section``
+`
+
+export const ArticleDeliveryPlaceConfirmationOrder = styled(BaseItensConfirmationOrder)`
+  p{
+    width:19.9375rem ;
+  }
+`;
+
+export const FigureLocale = styled.figure`
+  padding: 8px;
+  border-radius: 50%;
+  background: ${(props) => props.theme["--purple"]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  
+`;
+
+export const ArticleTimerConfirmationOrder = styled(BaseItensConfirmationOrder)`
+  p{
+    width: 8.8125rem;
+  }
+`;
+
+export const FigureTimer = styled.figure`
+  border-radius: 50%;
+  background: var(--brand-yellow, #dbac2c);
+  display: flex;
+  padding: 8px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const ArticleFormOfPaymentConfirmationOrder = styled(BaseItensConfirmationOrder)`
+  p{
+    width:10.125rem ;
+  }
+`;
+
+export const FigureFormOfPayment = styled.figure`
+  border-radius: 50%;
+  background: var(--brand-yellow-dark, #c47f17);
+  display: flex;
+  padding: 8px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+
+`;
+
+export const SectionIlustration = styled.section`
+  margin-bottom: -80px;
+`;
