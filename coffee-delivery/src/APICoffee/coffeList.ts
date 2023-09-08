cec8bf35abb86783723761564bd7a846fdeeaf19
ThaySaad <118492905+ThaySaad@users.noreply.github.com>
2023-09-08 16:27:32 -0300

    Create Context

--- coffee-delivery/src/APICoffee/coffeList.ts
+++ coffee-delivery/src/APICoffee/coffeList.ts
@@ -29,7 +29,9 @@ export const coffeeList = [
     type: ["tradicional"],
     name: "Expresso Tradicional",
     description: "O tradicional café feito com água quente e grãos moídos",
-    value: "9.90"
+    value: "9.90",
+    quantity: 0
+
   },
   {
     id: "2",
@@ -38,6 +40,7 @@ export const coffeeList = [
     name: "Expresso Americano",
     description: "Expresso diluído, menos intenso que o tradicional",
     value: "9.90",
+    quantity: 0
   },
   {
     id: "3",
@@ -46,6 +49,8 @@ export const coffeeList = [
     name: "Expresso Cremoso",
     description: "ECafé expresso tradicional com espuma cremosa",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "4",
@@ -54,6 +59,8 @@ export const coffeeList = [
     name: "Expresso Gelado",
     description: "Bebida preparada com café expresso e cubos de gelo",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "5",
@@ -62,6 +69,8 @@ export const coffeeList = [
     name: "Café com Leite",
     description: "Meio a meio de expresso tradicional com leite vaporizado",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "6",
@@ -71,6 +80,8 @@ export const coffeeList = [
     description:
       "Uma dose de café expresso com o dobro de leite e espuma cremosa",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "7",
@@ -80,6 +91,8 @@ export const coffeeList = [
     description:
       "Bebida com canela feita de doses iguais de café, leite e espuma",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "8",
@@ -88,6 +101,8 @@ export const coffeeList = [
     name: "Mocchiato",
     description: "Café expresso misturado com um pouco de leite quente e espuma",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "9",
@@ -96,6 +111,8 @@ export const coffeeList = [
     name: "Mocaccino",
     description: "Café expresso com calda de chocolate, pouco leite e espuma",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "10",
@@ -104,6 +121,8 @@ export const coffeeList = [
     name: "Chocolate Quente",
     description: "Bebida feita com chocolate dissolvido no leite quente e café",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "11",
@@ -113,6 +132,8 @@ export const coffeeList = [
     description:
       "Drink gelado de café expresso com rum, creme de leite e hortelã",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "12",
@@ -121,6 +142,8 @@ export const coffeeList = [
     name: "Havaiano",
     description: "Bebida adocicada preparada com café e leite de coco",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "13",
@@ -129,6 +152,8 @@ export const coffeeList = [
     name: "Árabe",
     description: "Bebida preparada com grãos de café árabe e especiarias",
     value: "9.90",
+    quantity: 0
+
   },
   {
     id: "14",
@@ -137,5 +162,7 @@ export const coffeeList = [
     name: "irlandês",
     description: "Bebida a base de café, uísque irlandês, açúcar e chantilly",
     value: "9.90",
+    quantity: 0
+
   },
 ];
---
+++ coffee-delivery/src/context/CoffeeContext.tsx
@@ -0,0 +1,7 @@
+export const CoffeeContext = () => {
+  return(
+    <div>
+      
+    </div>
+  )
+}
